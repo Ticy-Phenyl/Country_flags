9c9e6fb5485b2abbc538b78f4c1c4365146b5090
Ticy <68600934+Ticy-Phenyl@users.noreply.github.com>
2021-03-08 21:03:52 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+const searchInput = document.getElementById('search');
+const results = document.getElementById('result');
+
+let searchTerm = '';
+let Countries;
+
+
+
+//Api request 
+const fetchCountries = async() => {
+    countries = await fetch(
+        'http://restcountries.eu/rest/v2/all?fields=name;population;flag'
+    ).then(res => res.json());
+};
+
+const showCountries = async() => {
+    await fetchCountries();
+
+    results.innerHTML = (
+
+        countries
+            .filter(country => country.name.toLowerCase().includes(searchTerm.toLowerCase()))
+            .map(country => (
+
+                `
+                    <li class="country-item">
+                        <img class="country-flag" src= "${country.flag}" />
+                        <h3 class="country-name">${country.name}</h3>
+                        <div class="country-info">
+                            <h2 class="country-population">${numberWithSpace(country.population)}</h2>
+                            <h5 class="country-population-text">Habitants</h5>
+                        </div>    
+                    </li>                   
+                `
+            )).join('')
+
+    );
+
+};
+
+showCountries();
+
+//input setup
+
+searchInput.addEventListener('input', (e) => {searchTerm = e.target.value;
+    showCountries();
+});
+
+
+
+function numberWithSpace(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
